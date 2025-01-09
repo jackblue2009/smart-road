43a560f38f7a4e90bcb002d5e1e4b66a3fe4a822
abucheer <arahmanbucheeri@gmail.com>
2025-01-09 04:23:34 +0000

    still working on routing

--- src/world.rs
+++ src/world.rs
@@ -30,28 +30,6 @@ pub struct World {
 #[allow(dead_code)]
 impl World {
     pub fn new() -> Self {
-        // let mut traffic_lights = Vec::new();
-        // traffic_lights.push(TrafficLight::new(
-        //     380 - ROAD_WIDTH as i32 / 2,
-        //     320 - ROAD_WIDTH as i32,
-        //     0,
-        // )); // Top left
-        // traffic_lights.push(TrafficLight::new(
-        //     400 + ROAD_WIDTH as i32 / 2,
-        //     260 + ROAD_WIDTH as i32,
-        //     75,
-        // )); // Bottom right
-        // traffic_lights.push(TrafficLight::new(
-        //     420 - ROAD_WIDTH as i32,
-        //     380 - ROAD_WIDTH as i32 / 2,
-        //     150,
-        // )); // Bottom left
-        // traffic_lights.push(TrafficLight::new(
-        //     360 + ROAD_WIDTH as i32,
-        //     200 + ROAD_WIDTH as i32 / 2,
-        //     225,
-        // )); // Top right
-
         World {
             vehicles: Vec::new(),
             // traffic_lights,
@@ -62,9 +40,6 @@ impl World {
     }
 
     pub fn update(&mut self) {
-        // for light in &mut self.traffic_lights {
-        //     light.update();
-        // }
 
         for i in 0..self.vehicles.len() {
             let (current, others) = self.vehicles.split_at_mut(i);
@@ -81,14 +56,7 @@ impl World {
     pub fn draw(&self, canvas: &mut Canvas<Window>) -> Result<(), String> {
         // Draw roads
         canvas.set_draw_color(sdl2::pixels::Color::RGB(100, 100, 100));
-        // let lane_width = ROAD_WIDTH / 2;
         let lane_width = ROAD_WIDTH / 6;
-
-        // // Left lane (downward traffic)
-        // canvas.fill_rect(Rect::new(400 - ROAD_WIDTH as i32 / 2, 0, lane_width, 600))?;
-
-        // // Right lane (upward traffic)
-        // canvas.fill_rect(Rect::new(400, 0, lane_width, 600))?;
 
         // Vertical Road
         // Left side (downward traffic) - 3 lanes
@@ -101,12 +69,6 @@ impl World {
             canvas.fill_rect(Rect::new(400 + (i * lane_width as i32), 0, lane_width, 600))?;
         }
 
-        // // Top lane (leftward traffic)
-        // canvas.fill_rect(Rect::new(0, 300 - ROAD_WIDTH as i32 / 2, 800, lane_width))?;
-
-        // // Bottom lane (rightward traffic)
-        // canvas.fill_rect(Rect::new(0, 300, 800, lane_width))?;
-
         // Horizontal Road
         // Top side (leftward traffic) - 3 lanes
         for i in 0..3 {
@@ -117,29 +79,6 @@ impl World {
         for i in 0..3 {
             canvas.fill_rect(Rect::new(0, 300 + (i * lane_width as i32), 800, lane_width))?;
         }
-
-        // // Draw lane markings
-        // canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255)); // White color for lines
-
-        // // Vertical center lines (split into two parts to skip intersection)
-        // canvas.fill_rect(Rect::new(400 - 2, 0, 4, 300 - ROAD_WIDTH as u32 / 2))?; // Upper part
-
-        // canvas.fill_rect(Rect::new(
-        //     400 - 2,
-        //     300 + ROAD_WIDTH as i32 / 2,
-        //     4,
-        //     300 - ROAD_WIDTH as u32 / 2,
-        // ))?; // Lower part
-
-        // // Horizontal center lines (split into two parts to skip intersection)
-        // canvas.fill_rect(Rect::new(0, 300 - 2, 400 - ROAD_WIDTH as u32 / 2, 4))?; // Left part
-
-        // canvas.fill_rect(Rect::new(
-        //     400 + ROAD_WIDTH as i32 / 2,
-        //     300 - 2,
-        //     400 - ROAD_WIDTH as u32 / 2,
-        //     4,
-        // ))?; // Right part
 
         // Draw lane markings
         canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
@@ -163,10 +102,6 @@ impl World {
 
         // North end horizontal connectors
         canvas.fill_rect(Rect::new(
-            // 400 - ROAD_WIDTH as i32 / 2,  // Start from leftmost lane
-            // 100,                          // Some distance from the top
-            // ROAD_WIDTH / 2 as u32,            // Span across 3 lanes
-            // 4                             // Line thickness
             280,                          // Start from rightmost lane
             176,                          // Some distance from the bottom
             ROAD_WIDTH as u32 / 2,        // Span across right lanes
@@ -175,10 +110,6 @@ impl World {
 
         // South end horizontal connectors
         canvas.fill_rect(Rect::new(
-            // 400 - ROAD_WIDTH as i32 / 2,  // Start from leftmost lane
-            // 500,                          // Some distance from the bottom
-            // ROAD_WIDTH / 2 as u32,            // Span across 3 lanes
-            // 4                             // Line thickness
             400,                          // Start from rightmost lane
             420,                          // Some distance from the bottom
             ROAD_WIDTH as u32 / 2,        // Span across right lanes
@@ -187,10 +118,6 @@ impl World {
 
         // East end horizontal connectors
         canvas.fill_rect(Rect::new(
-            // 700,                          // Some distance from right edge
-            // 300 - ROAD_WIDTH as i32 / 2,  // Start from top lane
-            // 4,                            // Line thickness
-            // ROAD_WIDTH / 2 as u32             // Span across 3 lanes
             520,                          // Some distance from right edge
             180,                          // Start from rightmost lane
             4,                            // Line thickness
@@ -199,10 +126,6 @@ impl World {
 
         // West end horizontal connectors
         canvas.fill_rect(Rect::new(
-            // 100,                          // Some distance from left edge
-            // 300 - ROAD_WIDTH as i32 / 2,  // Start from top lane
-            // 4,                            // Line thickness
-            // ROAD_WIDTH / 2 as u32             // Span across 3 lanes
             277,                          // Some distance from left edge
             300,                          // Start from rightmost lane
             4,                            // Line thickness
@@ -213,11 +136,6 @@ impl World {
         for vehicle in &self.vehicles {
             vehicle.draw(canvas)?;
         }
-
-        // // Draw traffic lights
-        // for light in &self.traffic_lights {
-        //     light.draw(canvas)?;
-        // }
 
         Ok(())
     }
@@ -236,22 +154,13 @@ impl World {
 
         let mut rng = rand::thread_rng();
         let route = rng.gen_range(0..3);
+        // let route = (self.vehicles.len() % 3) as u8;
         let lane_width = ROAD_WIDTH / 6;
 
         // Define routing options for each direction
         let routing_options = [
             // Up arrow (spawn from South, moving North)
             VehicleRouting {
-                // spawn_x: 400 + (ROAD_WIDTH / 4) as i32, // Right lane
-                // spawn_y: 600,
-                // spawn_angle: -90.0,
-                // spawn_direction: 0,
-                // allowed_routes: &[0, 1, 2],
-                // spawn_x: 400 + (ROAD_WIDTH / 6 + route as u32 * ROAD_WIDTH / 6) as i32, // Right lanes based on route
-                // spawn_y: 600,
-                // spawn_angle: -90.0,
-                // spawn_direction: 0,
-                // allowed_routes: &[0, 1, 2],
                 spawn_x: 400 + ROAD_WIDTH as i32 / 2 - (lane_width as i32 * (route + 1) - lane_width as i32 / 2),
                 spawn_y: 600,
                 spawn_angle: -90.0,
@@ -260,16 +169,6 @@ impl World {
             },
             // Down arrow (spawn from North, moving South)
             VehicleRouting {
-                // spawn_x: 400 - (ROAD_WIDTH / 4) as i32, // Left lane
-                // spawn_y: 0,
-                // spawn_angle: 90.0,
-                // spawn_direction: 1,
-                // allowed_routes: &[0, 1, 2],
-                // spawn_x: 400 - (ROAD_WIDTH / 6 + route as u32 * ROAD_WIDTH / 6) as i32, // Left lanes based on route
-                // spawn_y: 0,
-                // spawn_angle: 90.0,
-                // spawn_direction: 1,
-                // allowed_routes: &[0, 1, 2],
                 spawn_x: 400 - ROAD_WIDTH as i32 / 2 + (lane_width as i32 * route + lane_width as i32 / 2),
                 spawn_y: 0,
                 spawn_angle: 90.0,
@@ -278,16 +177,6 @@ impl World {
             },
             // Left arrow (spawn from East, moving West)
             VehicleRouting {
-                // spawn_x: 800,
-                // spawn_y: 300 - (ROAD_WIDTH / 4) as i32, // Top lane
-                // spawn_angle: 180.0,
-                // spawn_direction: 2,
-                // allowed_routes: &[0, 1, 2],
-                // spawn_x: 800,
-                // spawn_y: 300 - (ROAD_WIDTH / 6 + route as u32 * ROAD_WIDTH / 6) as i32, // Top lanes based on route
-                // spawn_angle: 180.0,
-                // spawn_direction: 2,
-                // allowed_routes: &[0, 1, 2],
                 spawn_x: 800,
                 spawn_y: 300 - ROAD_WIDTH as i32 / 2 + (lane_width as i32 * route + lane_width as i32 / 2),
                 spawn_angle: 180.0,
@@ -296,71 +185,114 @@ impl World {
             },
             // Right arrow (spawn from West, moving East)
             VehicleRouting {
-                // spawn_x: 0,
-                // spawn_y: 300 + (ROAD_WIDTH / 4) as i32, // Bottom lane
-                // spawn_angle: 0.0,
-                // spawn_direction: 3,
-                // allowed_routes: &[0, 1, 2],
-                // spawn_x: 0,
-                // spawn_y: 300 + (ROAD_WIDTH / 6 + route as u32 * ROAD_WIDTH / 6) as i32, // Bottom lanes based on route
-                // spawn_angle: 0.0,
-                // spawn_direction: 3,
-                // allowed_routes: &[0, 1, 2],
                 spawn_x: 0,
                 spawn_y: 300 + ROAD_WIDTH as i32 / 2 - (lane_width as i32 * (route + 1) - lane_width as i32 / 2),
                 spawn_angle: 0.0,
                 spawn_direction: 3,
                 allowed_routes: &[0, 1, 2],
             },
+
+            // // Up arrow (spawn from South, moving North)
+            // VehicleRouting {
+            //     spawn_x: 400 + ROAD_WIDTH as i32 / 2 - (lane_width as i32 * (2-route as i32) - lane_width as i32 / 2),
+            //     spawn_y: 600,
+            //     spawn_angle: -90.0,
+            //     spawn_direction: 0,
+            //     allowed_routes: &[route],
+            // },
+            // // Down arrow (spawn from North, moving South)
+            // VehicleRouting {
+            //     spawn_x: 400 - ROAD_WIDTH as i32 / 2 + (lane_width as i32 * route as i32 + lane_width as i32 / 2),
+            //     spawn_y: 0,
+            //     spawn_angle: 90.0,
+            //     spawn_direction: 1,
+            //     allowed_routes: &[route],
+            // },
+            // // Left arrow (spawn from East, moving West) 
+            // VehicleRouting {
+            //     spawn_x: 800,
+            //     spawn_y: 300 - ROAD_WIDTH as i32 / 2 + (lane_width as i32 * route as i32 + lane_width as i32 / 2),
+            //     spawn_angle: 180.0,
+            //     spawn_direction: 2,
+            //     allowed_routes: &[route],
+            // },
+            // // Right arrow (spawn from West, moving East)
+            // VehicleRouting {
+            //     spawn_x: 0,
+            //     spawn_y: 300 + ROAD_WIDTH as i32 / 2 - (lane_width as i32 * (2-route as i32) - lane_width as i32 / 2),
+            //     spawn_angle: 0.0,
+            //     spawn_direction: 3,
+            //     allowed_routes: &[route],
+            // },
         ];
 
         // Match keycode to appropriate routing
         match keycode {
-            Keycode::Up => {
-                let route = routing_options[0].allowed_routes
-                    [rng.gen_range(0..routing_options[0].allowed_routes.len())];
+            // Keycode::Up => {
+            //     let route = routing_options[0].allowed_routes
+            //         [rng.gen_range(0..routing_options[0].allowed_routes.len())];
+            //     self.vehicles.push(Vehicle::new(
+            //         routing_options[0].spawn_x,
+            //         routing_options[0].spawn_y,
+            //         routing_options[0].spawn_angle,
+            //         routing_options[0].spawn_direction,
+            //         route,
+            //     ));
+            //     self.last_vehicle_spawn_time = Instant::now();
+            // }
+            // Keycode::Down => {
+            //     let route = routing_options[1].allowed_routes
+            //         [rng.gen_range(0..routing_options[1].allowed_routes.len())];
+            //     self.vehicles.push(Vehicle::new(
+            //         routing_options[1].spawn_x,
+            //         routing_options[1].spawn_y,
+            //         routing_options[1].spawn_angle,
+            //         routing_options[1].spawn_direction,
+            //         route,
+            //     ));
+            //     self.last_vehicle_spawn_time = Instant::now();
+            // }
+            // Keycode::Left => {
+            //     let route = routing_options[2].allowed_routes
+            //         [rng.gen_range(0..routing_options[2].allowed_routes.len())];
+            //     self.vehicles.push(Vehicle::new(
+            //         routing_options[2].spawn_x,
+            //         routing_options[2].spawn_y,
+            //         routing_options[2].spawn_angle,
+            //         routing_options[2].spawn_direction,
+            //         route,
+            //     ));
+            //     self.last_vehicle_spawn_time = Instant::now();
+            // }
+            // Keycode::Right => {
+            //     let route = routing_options[3].allowed_routes
+            //         [rng.gen_range(0..routing_options[3].allowed_routes.len())];
+            //     self.vehicles.push(Vehicle::new(
+            //         routing_options[3].spawn_x,
+            //         routing_options[3].spawn_y,
+            //         routing_options[3].spawn_angle,
+            //         routing_options[3].spawn_direction,
+            //         route,
+            //     ));
+            //     self.last_vehicle_spawn_time = Instant::now();
+            // }
+            Keycode::Up | Keycode::Down | Keycode::Left | Keycode::Right => {
+                let direction_index = match keycode {
+                    Keycode::Up => 0,
+                    Keycode::Down => 1,
+                    Keycode::Left => 2,
+                    Keycode::Right => 3,
+                    _ => unreachable!()
+                };
+                // Cycle through lanes 0,1,2 (right, straight, left)
+                let route = (self.vehicles.len() % 3) as u8;
+                let routing = &routing_options[direction_index];
                 self.vehicles.push(Vehicle::new(
-                    routing_options[0].spawn_x,
-                    routing_options[0].spawn_y,
-                    routing_options[0].spawn_angle,
-                    routing_options[0].spawn_direction,
-                    route,
-                ));
-                self.last_vehicle_spawn_time = Instant::now();
-            }
-            Keycode::Down => {
-                let route = routing_options[1].allowed_routes
-                    [rng.gen_range(0..routing_options[1].allowed_routes.len())];
-                self.vehicles.push(Vehicle::new(
-                    routing_options[1].spawn_x,
-                    routing_options[1].spawn_y,
-                    routing_options[1].spawn_angle,
-                    routing_options[1].spawn_direction,
-                    route,
-                ));
-                self.last_vehicle_spawn_time = Instant::now();
-            }
-            Keycode::Left => {
-                let route = routing_options[2].allowed_routes
-                    [rng.gen_range(0..routing_options[2].allowed_routes.len())];
-                self.vehicles.push(Vehicle::new(
-                    routing_options[2].spawn_x,
-                    routing_options[2].spawn_y,
-                    routing_options[2].spawn_angle,
-                    routing_options[2].spawn_direction,
-                    route,
-                ));
-                self.last_vehicle_spawn_time = Instant::now();
-            }
-            Keycode::Right => {
-                let route = routing_options[3].allowed_routes
-                    [rng.gen_range(0..routing_options[3].allowed_routes.len())];
-                self.vehicles.push(Vehicle::new(
-                    routing_options[3].spawn_x,
-                    routing_options[3].spawn_y,
-                    routing_options[3].spawn_angle,
-                    routing_options[3].spawn_direction,
-                    route,
+                    routing.spawn_x,
+                    routing.spawn_y,
+                    routing.spawn_angle,
+                    routing.spawn_direction,
+                    route
                 ));
                 self.last_vehicle_spawn_time = Instant::now();
             }
